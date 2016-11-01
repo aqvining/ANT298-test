af1ce5aeffe847205336a16643e1af5f8c58c5c7
FosseyFund <Damien@campus-116-068.ucdavis.edu>
2016-11-01 13:58:59 -0700

    added linear model simulation

--- myScript.R
+++ myScript.R
@@ -2,3 +2,15 @@
 
 x <- rnorm(n = 1000, mean=0, sd=3)
 hist(x, nclass=20)
+
+###
+m <- 3
+b <- -2
+eps <- rnorm(1000,mean=0,sd=50)
+
+x <- runif(1000, 0,100)
+y <- m*x+b+eps
+
+plot(x,y, cex=.5, col="blue")
+########
+
